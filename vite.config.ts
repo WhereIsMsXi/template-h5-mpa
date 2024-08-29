f87e813e94e031bdb7b53e824babb2d76e814876
WhereIsMsXi <whereismsxi@gmail.com>
2024-08-29 10:24:19 +0800

    feat(custom): 完成打包mpa的单页面

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
-import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
+import { fileURLToPath, URL } from 'node:url';
 import vue from '@vitejs/plugin-vue';
 
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from '@vant/auto-import-resolver';
+
+import { currentEntry } from './config';
 
 // @ts-ignore
 import pxtovw from 'postcss-px-to-viewport';
@@ -16,29 +18,37 @@ const loderPxtovw = pxtovw({
 });
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: '@import "@where_is_mr_li/style/dist/mixin.scss";',
+export default (mode: any) => {
+  const tempMode = mode.mode;
+  return defineConfig({
+    root: resolve(__dirname, currentEntry.src),
+    build: {
+      outDir: resolve(__dirname, currentEntry.name),
+    },
+    base: tempMode === 'development' ? './' : currentEntry.ftp,
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@where_is_mr_li/style/dist/mixin.scss";',
+        },
+      },
+      postcss: {
+        plugins: [loderPxtovw],
       },
     },
-    postcss: {
-      plugins: [loderPxtovw],
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [VantResolver()],
+      }),
+      Components({
+        resolvers: [VantResolver()],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [VantResolver()],
-    }),
-    Components({
-      resolvers: [VantResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-});
+  });
+};
