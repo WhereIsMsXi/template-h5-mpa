153453ff133f0648bcc45b54edaba67be609e90b
WhereIsMsXi <whereismsxi@gmail.com>
2024-08-29 09:58:53 +0800

    feat(custom): 添加mpa的通用样式

--- src/views/my-page/main.ts
+++ src/views/my-page/main.ts
@@ -1,6 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import '@where_is_mr_li/style/dist/index.scss';
+import './styles/index.scss';
+
 function boot() {
   const app = createApp(App);
 
